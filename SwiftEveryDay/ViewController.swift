531013788e9df86e319283e4e868d8e03f5f9c39
Hao Zheng <babyshung8@gmail.com>
2015-10-26 23:59:09 -0400

    Initial Commit

---
+++ SwiftEveryDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftEveryDay
+//
+//  Created by Hao Zheng on 10/26/15.
+//  Copyright © 2015 Planhola.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
