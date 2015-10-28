af6a769dce7867b7bf2279bbd64c9e4389c3d67f
Hao Zheng <babyshung8@gmail.com>
2015-10-28 00:03:36 -0400

    map

--- SwiftEveryDay/ViewController.swift
+++ SwiftEveryDay/ViewController.swift
@@ -13,12 +13,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let array = [1, 2, 3]
+        let transformedArray = array.map { element in
+            element * 4
+        }
+        print(transformedArray)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
