6a0d5d35f40011b337f241887a5612982574fa9d
Hao Zheng <babyshung8@gmail.com>
2015-10-29 00:21:56 -0400

    reduce

--- SwiftEveryDay/ViewController.swift
+++ SwiftEveryDay/ViewController.swift
@@ -19,6 +19,12 @@ class ViewController: UIViewController {
             element * 4
         }
         print(transformedArray)
+        
+        //reduce
+        let reduceArray = [1, 3, 5]
+        let reduced = reduceArray.reduce("") { str, num in str + "\(num)\n"
+        }
+        print(reduced)
     }
 
     override func didReceiveMemoryWarning() {
